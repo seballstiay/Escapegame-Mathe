df9abeedd01bf64c053e6a29566a9e9f516f43f5
Simon <simon.weckler@mnet-online.de>
2022-07-13 12:20:14 +0200

    feat: Aufgabe 1 Hinweis Anfang

---
+++ src/app/Aufgaben/aufgabe3/aufgabe3.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Aufgabe3Component } from './aufgabe3.component';
+
+describe('Aufgabe3Component', () => {
+  let component: Aufgabe3Component;
+  let fixture: ComponentFixture<Aufgabe3Component>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ Aufgabe3Component ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(Aufgabe3Component);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/Aufgaben/aufgabe3/aufgabe3.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-aufgabe3',
+  templateUrl: './aufgabe3.component.html',
+  styleUrls: ['./aufgabe3.component.scss']
+})
+export class Aufgabe3Component implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
