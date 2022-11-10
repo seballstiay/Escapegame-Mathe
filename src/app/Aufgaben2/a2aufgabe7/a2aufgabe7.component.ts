84464f827308882285f0445e25a1f1ec2cbced2f
BigInJapam <maringerfelix25@gmail.com>
2022-11-10 17:22:21 +0100

    A2A7 test

--- src/app/Aufgaben2/a2aufgabe7/a2aufgabe7.component.ts
+++ src/app/Aufgaben2/a2aufgabe7/a2aufgabe7.component.ts
@@ -9,20 +9,64 @@ export class A2aufgabe7Component implements OnInit {
   constructor(private router: Router) {}
 
   $a = 0;
-  inputWert = '';
+  $b = 0;
+  images = ["\assets\images\QRCodeDef.png", "\assets\images\QRCodeA1.png", "\assets\images\QRCodeA2.png",
+             "\assets\images\QRCodeA3.png", "\assets\images\QRCodeA4.png", "\assets\images\QRCodeA5.png",];
+  image = this.images[0];
+  inputWert1 = '';
+  inputWert2 = '';
+  inputWert3 = '';
+  inputWert4 = '';
+  inputWert5 = '';
   hinweise = {
     0: false,
     1: false,
     2: false
   };
   //Hier kommt das richtige Ergebnis der Aufgabe hin
-  $antwort = '2g';
+  $antwort1 = '7';
+  $antwort2 = '4';
+  $antwort3 = '5';
+  $antwort4 = '1';
+  $antwort5 = '9';
 
   ngOnInit(): void {}
+  bildW() {
+    if(this.inputWert1 === this.$antwort1){
+        this.image = this.images[1];
+    }
+    if(this.inputWert1 === this.$antwort1 &&
+      this.inputWert2 === this.$antwort2 ){
+      this.image = this.images[2];
+    }
+    if(this.inputWert1 === this.$antwort1 &&
+      this.inputWert2 === this.$antwort2 &&
+      this.inputWert3 === this.$antwort3 ){
+      this.image = this.images[3];
+    }
+    if(this.inputWert1 === this.$antwort1 &&
+      this.inputWert2 === this.$antwort2 &&
+      this.inputWert3 === this.$antwort3 &&
+      this.inputWert4 === this.$antwort4 ){
+      this.image = this.images[4];
+    }
+    if(this.inputWert1 === this.$antwort1 &&
+      this.inputWert2 === this.$antwort2 &&
+      this.inputWert3 === this.$antwort3 &&
+      this.inputWert4 === this.$antwort4 &&
+      this.inputWert5 === this.$antwort5 ){
+      this.image = this.images[5];
+    }
+
+  }
 
   pruefen() {
-    if (this.inputWert === this.$antwort) {
-      this.router.navigate(['/spiel-beitreten']);
+    if (this.inputWert1 === this.$antwort1 &&
+        this.inputWert2 === this.$antwort2 &&
+        this.inputWert3 === this.$antwort3 &&
+        this.inputWert4 === this.$antwort4 &&
+        this.inputWert5 === this.$antwort5) {
+      this.router.navigate(['/a2aufgabe5']);
     } else {
       console.log('Dieses Ergebnis ist flasch');
       this.$a++;
