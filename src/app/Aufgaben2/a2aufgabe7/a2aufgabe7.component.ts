6b53702a1364328958998d7db001f4884c8da89e
BigInJapam <maringerfelix25@gmail.com>
2022-11-13 14:54:33 +0100

    Neue QR-Bilder, Test-QR fertig, icon, Welt 3 Audio

--- src/app/Aufgaben2/a2aufgabe7/a2aufgabe7.component.ts
+++ src/app/Aufgaben2/a2aufgabe7/a2aufgabe7.component.ts
@@ -10,9 +10,11 @@ export class A2aufgabe7Component implements OnInit {
 
   $a = 0;
   $b = 0;
-  images = ["\\assets\\images\\QRCodeDef.png", "\\assets\\images\\QRCodeA1.png", "\\assets\\images\\QRCodeA2.png",
-             "\\assets\\images\\QRCodeA3.png", "\\assets\\images\\QRCodeA4.png", "\\assets\\images\\QRCodeA5.png"];
-  image = this.images[0];
+  images = ["\\assets\\images\\QR\\QR1.PNG", "\\assets\\images\\QR\\QR2.PNG", "\\assets\\images\\QR\\QR3.PNG",
+             "\\assets\\images\\QR\\QR4.PNG", "\\assets\\images\\QR\\QR5.PNG", "\\assets\\images\\QR\\QR6.PNG"];
+  indexI = 0;
+  image = this.images[this.indexI];
+  
   inputWert1 = '';
   inputWert2 = '';
   inputWert3 = '';
@@ -33,31 +35,26 @@ export class A2aufgabe7Component implements OnInit {
   ngOnInit(): void {}
   bildW() {
     if(this.inputWert1 === this.$antwort1){
-      this.image = this.images[1];
+      this.$antwort1 += 1;
+      this.indexI = this.indexI+1;
     }
-    if(this.inputWert1 === this.$antwort1 &&
-      this.inputWert2 === this.$antwort2 ){
-      this.image = this.images[2];
+    if(this.inputWert2 === this.$antwort2 ){
+      this.$antwort2 += 1;
+      this.indexI = this.indexI+1;
     }
-    if(this.inputWert1 === this.$antwort1 &&
-      this.inputWert2 === this.$antwort2 &&
-      this.inputWert3 === this.$antwort3 ){
-      this.image = this.images[3];
+    if(this.inputWert3 === this.$antwort3 ){
+      this.$antwort3 += 1;
+      this.indexI = this.indexI+1;
     }
-    if(this.inputWert1 === this.$antwort1 &&
-      this.inputWert2 === this.$antwort2 &&
-      this.inputWert3 === this.$antwort3 &&
-      this.inputWert4 === this.$antwort4 ){
-      this.image = this.images[4];
+    if(this.inputWert4 === this.$antwort4 ){
+      this.$antwort4 += 1;
+      this.indexI = this.indexI+1;
     }
-    if(this.inputWert1 === this.$antwort1 &&
-      this.inputWert2 === this.$antwort2 &&
-      this.inputWert3 === this.$antwort3 &&
-      this.inputWert4 === this.$antwort4 &&
-      this.inputWert5 === this.$antwort5 ){
-      this.image = this.images[5];
+    if(this.inputWert5 === this.$antwort5 ){
+      this.$antwort5 += 1;
+      this.indexI = this.indexI+1;
     }
-
+    this.image=this.images[this.indexI];
   }
 
   bilder():string{
@@ -65,11 +62,11 @@ export class A2aufgabe7Component implements OnInit {
   }
 
   pruefen() {
-    if (this.inputWert1 === this.$antwort1 &&
-        this.inputWert2 === this.$antwort2 &&
-        this.inputWert3 === this.$antwort3 &&
-        this.inputWert4 === this.$antwort4 &&
-        this.inputWert5 === this.$antwort5) {
+    if (this.inputWert1+1 === this.$antwort1 &&
+        this.inputWert2+1 === this.$antwort2 &&
+        this.inputWert3+1 === this.$antwort3 &&
+        this.inputWert4+1 === this.$antwort4 &&
+        this.inputWert5+1 === this.$antwort5) {
       this.router.navigate(['/a2aufgabe5']);
     } else {
       console.log('Dieses Ergebnis ist flasch');
